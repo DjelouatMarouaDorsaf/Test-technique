0e9d4de8dec1b56b2462f1d3fa066dcd6aa64921
Djelouat Maroua Dorsaf <maroua-dorsaf.djelouat@etu.umontpellier.fr>
2021-12-04 19:21:38 +0100

    modifications affichage

--- Exo2.py
+++ Exo2.py
@@ -21,7 +21,9 @@ def DPMR(tab, i, j, X):
 
 
 print('Nombre à insérer : ' + str(X))
+print("______________")
 print('tabelau trié avant insertion : '+ str(tab))
+print("______________")
 result=DPMR(tab,0,len(tab)-1,X)
 tab.insert(result, X)
 print('tabelau trié après insertion : '+ str(tab))
